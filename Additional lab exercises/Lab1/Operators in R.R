b19db42d8eca81480815fb7afde0346dbc136b67
Ayush bankar <ayushbankar626@gmail.com>
2025-11-05 22:30:41 +0530

    Create Operators in R.R

---
+++ Additional lab exercises/Lab1/Operators in R.R
@@ -0,0 +1,58 @@
+# 1. Perform the following operations:
+
+# a. Addition
+print(10 + 5)
+
+# b. Subtraction
+print(10 - 5)
+
+# c. Multiplication
+print(10 * 5)
+
+# d. Division
+print(10 / 5)
+
+# e. Modulus (remainder)
+print(10 %% 3)
+
+# f. Integer Division (quotient)
+print(10 %/% 3)
+
+# 2. Compare if 15 is greater than 10, and if 7 is equal to 7.
+print((15 > 10) && (7 == 7))
+
+# 3. # 3. Create two vectors:
+a <- c(2, 4, 6, 8)
+b <- c(1, 3, 5, 7)
+
+# i. Addition (Element-wise)
+print(a + b)
+
+# ii. Subtraction (Element-wise)
+print(a - b)
+
+# iii. Element-wise multiplication
+print(a * b)
+
+# 4. Use logical operators to check:
+# a. Which elements of a are greater than 5?
+print(a > 5)
+
+# b. Which elements of b are less than or equal to 4?
+print(b <= 4)
+
+# 5. Use %in% to check if the number 5 exists in vector a.
+print(5 %in% a)
+
+# 6. Given boolean vectors x and y, apply logical operators.
+x <- c(TRUE, FALSE, TRUE, FALSE)
+y <- c(TRUE, TRUE, FALSE, FALSE)
+
+# a. Element-wise AND (&)
+print(x & y)
+
+# b. Element-wise OR (|)
+print(x | y)
+
+# c. Element-wise NOT (!) for vector x
+print(!x)
